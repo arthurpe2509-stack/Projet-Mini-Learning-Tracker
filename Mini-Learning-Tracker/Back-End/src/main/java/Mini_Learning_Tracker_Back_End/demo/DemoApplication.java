fe220a8f96470d9881380003fd0a5e10a10376fc
Arthur Pereira <art-pereira@smacl.fr>
2026-06-30 13:29:13 +0200

    Back-End

---
+++ Mini-Learning-Tracker/Back-End/src/main/java/Mini_Learning_Tracker_Back_End/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Mini_Learning_Tracker_Back_End.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ Mini-Learning-Tracker/Back-End/src/test/java/Mini_Learning_Tracker_Back_End/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Mini_Learning_Tracker_Back_End.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
