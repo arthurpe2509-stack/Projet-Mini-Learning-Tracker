6385f6a75ef652b7583d28ee23dd6000501e7ecc
Arthur Pereira <art-pereira@smacl.fr>
2026-07-01 11:40:39 +0200

    component

---
+++ Mini-Learning-Tracker/Front-End/src/app/component/ressources/form-ressource/form-ressource.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-ressource',
+  imports: [],
+  templateUrl: './form-ressource.html',
+  styleUrl: './form-ressource.css',
+})
+export class FormRessource {}
--- Mini-Learning-Tracker/Front-End/src/app/component/ressources/id/id.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-id',
-  imports: [],
-  templateUrl: './id.html',
-  styleUrl: './id.css',
-})
-export class Id {}
