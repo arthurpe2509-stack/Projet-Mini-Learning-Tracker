f44133e4a8c286f9b5b37172ae8d9e783804fb3d
Guibert Vincent <v-guibert@smacl.fr>
2026-07-01 13:50:03 +0200

    feat: creation service

--- Mini-Learning-Tracker/Back-End/src/main/java/Mini_Learning_Tracker_Back_End/repositories/RessourceRepository.java
+++ Mini-Learning-Tracker/Back-End/src/main/java/Mini_Learning_Tracker_Back_End/repositories/RessourceRepository.java
@@ -8,4 +8,5 @@ import java.util.UUID;
 
 @Repository
 public interface RessourceRepository extends JpaRepository<RessourceEntity, UUID> {
+    RessourceEntity findRessourceEntityByRessourceId(UUID ressourceId);
 }
